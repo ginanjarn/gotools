2b734a3cbc861ca2db8972e25b667b2cb80c7d02
ginanjarn <ginanjarn@gmail.com>
2021-07-22 16:13:51 +0700

    minimal documentation

--- gotools.py
+++ gotools.py
@@ -48,6 +48,41 @@ def get_completion(source: str, workdir: str, location: int):
         logger.error(err)
 
 
+def get_documentation(source: str, workdir: str, location: int):
+
+    command = ["gocode", "-f=csv", "autocomplete", "c%s" % location]
+    env = os.environ.copy()
+
+    if os.name == "nt":
+        # STARTUPINFO only available on windows
+        startupinfo = subprocess.STARTUPINFO()
+        startupinfo.dwFlags |= subprocess.SW_HIDE | subprocess.STARTF_USESHOWWINDOW
+    else:
+        startupinfo = None
+
+    try:
+        process = subprocess.Popen(
+            command,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            startupinfo=startupinfo,
+            shell=True,
+            env=env,
+            cwd=workdir,
+        )
+        sout, serr = process.communicate(source.encode("utf8"))
+        if serr:
+            logger.debug(
+                "completion error:\n%s" % ("\n".join(serr.decode().splitlines()))
+            )
+            return None
+        return sout.decode("utf8")
+
+    except OSError as err:
+        logger.error(err)
+
+
 GocodeResult = namedtuple("GocodeResult", ["type_", "name", "data", "package"])
 
 
@@ -102,6 +137,48 @@ class Completion:
         return cls(completions)
 
 
+class Documentation:
+    def __init__(self, doc: str, *, package: str = "", methodOrField: str = ""):
+        self.documentation = doc
+        self.pkg_method = "%s%s" % (
+            package,
+            "" if not methodOrField else "%s" % methodOrField,
+        )
+
+    @classmethod
+    def from_gocode_csv(cls, raw: str):
+        """parse from gocode csv"""
+
+        candidates = raw.splitlines()
+        result = GocodeResult(*candidates[0].split(",,"))
+        logger.debug(result)
+
+        if result.name == "main":
+            return cls(doc="")
+
+        if result.data == "invalid type":
+            return cls(doc="")
+
+        doc_template = "<body style='padding: 0.5em;'><strong>{head}</strong><div><p>{body}</p><a href='#'>More</a></div></body>"
+
+        if result.type_ == "package":
+            doc = doc_template.format(
+                head=result.name, body="package %s" % (result.name)
+            )
+            return cls(doc, package=result.name)
+
+        if result.type_ == "func":
+            head = "%s.%s" % (result.package, result.name)
+            body = "%s%s" % (result.name, result.data[4:])
+            doc = doc_template.format(head=head, body=body)
+            return cls(doc, package=result.package, methodOrField=result.name)
+
+        head = "%s.%s" % (result.package, result.name)
+        body = "%s %s %s" % (result.type_, result.name, result.data)
+        doc = doc_template.format(head=head, body=body)
+        return cls(doc, package=result.package, methodOrField=result.name)
+
+
 def valid_source(view: sublime.View, location: int = 0) -> bool:
     """valid go source code"""
     return view.match_selector(location, "source.go")
@@ -111,6 +188,9 @@ def valid_scope(view: sublime.View, location: int) -> bool:
     """valid scope for completion"""
 
     if view.match_selector(location, "source.go string"):
+        return False
+
+    if view.match_selector(location, "source.go comment"):
         return False
 
     return True
@@ -132,6 +212,24 @@ def show_completions(view: sublime.View) -> None:
 def hide_completions(view: sublime.View) -> None:
     """Opens (forced) the sublime autocomplete window"""
     view.run_command("hide_auto_complete")
+
+
+def show_popup(
+    view: sublime.View,
+    content,
+    flags=0,
+    location=-1,
+    max_width=1024,
+    max_height=480,
+    on_navigate=None,
+    on_hide=None,
+):
+    if not flags:
+        flags = sublime.HIDE_ON_MOUSE_MOVE_AWAY | sublime.COOPERATE_WITH_AUTO_COMPLETE
+
+    view.show_popup(
+        content, flags, location, max_width, max_height, on_navigate, on_hide
+    )
 
 
 PLUGIN_ENABLED = False
@@ -183,3 +281,29 @@ class Event(sublime_plugin.ViewEventListener):
             enable_plugin()
         else:
             enable_plugin(False)
+
+    def get_documentation(self, view: sublime.View, location: int):
+        end = view.word(location).b
+        source = view.substr(sublime.Region(0, view.size()))
+        workdir = os.path.dirname(view.file_name())
+        candidates = get_documentation(source, workdir, end)
+        if not candidates:
+            return
+
+        doc = Documentation.from_gocode_csv(candidates)
+
+        if doc.documentation:
+            show_popup(view, content=doc.documentation, location=location)
+
+    def on_hover(self, point: int, hover_zone: int):
+        if not PLUGIN_ENABLED:
+            return
+
+        if not valid_scope(self.view, point):
+            return
+
+        if hover_zone == sublime.HOVER_TEXT:
+            thread = threading.Thread(
+                target=self.get_documentation, args=(self.view, point)
+            )
+            thread.start()
