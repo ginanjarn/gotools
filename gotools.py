c1210220a37ad9e039fc9a3b80bb51eab53ef152
ginanjarn <ginanjarn@gmail.com>
2022-05-09 21:41:31 +0700

    prevent clean diagnostic on save

--- gotools.py
+++ gotools.py
@@ -1133,8 +1133,8 @@ class EventListener(sublime_plugin.EventListener):
 
         try:
             GOPLS_CLIENT.textDocument_didSave(file_name)
-            document = Document(file_name)
-            document.clear_diagnostics()
+            # document = Document(file_name)
+            # document.clear_diagnostics()
 
         except ServerOffline:
             pass
